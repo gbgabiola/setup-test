a7590aa545923204541f44f353bb1a2d56f0fa4f
Genesis Gabiola <gbgabiola@gmail.com>
2021-08-15 14:59:55 +0800

    Add short circuit condition when showing elements

--- src/App.js
+++ src/App.js
@@ -18,11 +18,12 @@ const emojis = [
 
 function App() {
   const greeting = 'greeting';
+  const displayAction = false;
 
   return (
     <div className="container">
       <h1 id={greeting}>Hello, World</h1>
-      <p>I am writing JSX</p>
+      {displayAction && <p>I am writing JSX</p>}
       <ul>
         {emojis.map(emoji => (
           <li key={emoji.name}>
@@ -33,27 +34,6 @@ function App() {
             </button>
           </li>
         ))}
-        {/* <li>
-          <button onClick={displayEmojiName}>
-            <span role="img" aria-label="grinning face" id="grinning face">
-              😀
-            </span>
-          </button>
-        </li> */}
-        {/* <li>
-          <button onClick={displayEmojiName}>
-            <span role="img" aria-label="party popper" id="party popper">
-              🎉
-            </span>
-          </button>
-        </li>
-        <li>
-          <button onClick={displayEmojiName}>
-            <span role="img" aria-label="woman dancing" id="woman dancing">
-              💃
-            </span>
-          </button>
-        </li> */}
       </ul>
     </div>
   );
