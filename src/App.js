3c2dff5823596c9c9c9c25731f6effb028e21a3d
Genesis Gabiola <gbgabiola@gmail.com>
2021-08-15 14:09:06 +0800

    Add style to an element with attributes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import './App.css';
 
 function App() {
+  const greeting = 'greeting';
+
   return (
-    <>
-      <h1>Hello, World</h1>
+    <div className="container">
+      <h1 id={greeting}>Hello, World</h1>
       <p>I am writing JSX</p>
-    </>
+      <ul>
+        <li>
+          <button>
+            <span role="img" aria-label="grinning face" id="grinning face">
+              😀
+            </span>
+          </button>
+        </li>
+        <li>
+          <button>
+            <span role="img" aria-label="party popper" id="party popper">
+              🎉
+            </span>
+          </button>
+        </li>
+        <li>
+          <button>
+            <span role="img" aria-label="woman dancing" id="woman dancing">
+              💃
+            </span>
+          </button>
+        </li>
+      </ul>
+    </div>
   );
 }
 
