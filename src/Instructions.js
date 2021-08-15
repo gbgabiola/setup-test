421738fdb4a70986c93edae6d891e1c3b760299b
Genesis Gabiola <gbgabiola@gmail.com>
2021-08-15 16:51:33 +0800

    Add an independent component with react classes

---
+++ src/Instructions.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import emoji from './emoji.svg';
+
+export default class Instructions extends Component {
+  render() {
+    return (
+      <div className="instructions">
+        <img src={emoji} alt="laughing crying emoji" />
+        <p>Click on an emoji to view the emoji short name.</p>
+      </div>
+    );
+  }
+}
