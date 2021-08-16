915616664f7ed0b228fdf98fa31cc5832791718f
Genesis Gabiola <gbgabiola@gmail.com>
2021-08-16 23:44:47 +0800

    Convert class-based component into functional component

--- src/components/Instructions/Instructions.js
+++ src/components/Instructions/Instructions.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Instructions.css';
 import emoji from './emoji.svg';
 
-export default class Instructions extends Component {
-  render() {
-    return (
-      <div className="instructions">
-        <img src={emoji} alt="laughing crying emoji" />
-        <p>Click on an emoji to view the emoji short name.</p>
-      </div>
-    );
-  }
-}
+const Instructions = () => {
+  return (
+    <div className="instructions">
+      <img src={emoji} alt="laughing crying emoji" />
+      <p>Click on an emoji to view the emoji short name.</p>
+    </div>
+  );
+};
+
+export default Instructions;
