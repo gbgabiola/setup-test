6fab2ea3d1adc0d721a2686410f42f64c44cd8d3
Genesis Gabiola <gbgabiola@gmail.com>
2021-08-18 21:22:34 +0800

    Pass data to components using props

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const Body = () => (
-  <p className="App-intro">
-    To get started, edit <code>src/App.js</code> and save to reload.
-  </p>
+const Body = props => (
+  <div>
+    <p className="App-intro">{props.text}</p>
+    <p className="App-intro">{props.text2}</p>
+    <p className="App-intro">{props.myFunc(1, 2)}</p>
+  </div>
 );
 
 // const Body = () => {
@@ -29,17 +31,34 @@ class Header extends Component {
     return (
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
+        <h1 className="App-title">{this.props.title}</h1>
+        <div>{this.props.myFunc(10, 12)}</div>
       </header>
     );
   }
 }
 class App extends Component {
+  add(a, b) {
+    return a + b;
+  }
   render() {
+    // const add = (a, b) => a + b;
+
     return (
       <div className="App">
-        <Header />
-        <Body />
+        <Header
+          title="Hello"
+          num={5}
+          myArr={[1, 2, 3]}
+          // myFunc={(a, b) => a + b}
+          myFunc={this.add}
+          myobj={{
+            a: 5,
+            b: 6,
+          }}
+        />
+        <Body myFunc={this.add} text={'I am cool.'} text2={'I am cool2.'} />
+        <Body myFunc={this.add} text={'I am cool.'} text2={'I am cool2.'} />
       </div>
     );
   }
