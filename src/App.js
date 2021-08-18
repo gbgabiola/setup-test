9854846aad499a943aa4af66f773996552f8e36c
Genesis Gabiola <gbgabiola@gmail.com>
2021-08-18 20:34:27 +0800

    Create custom components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+const Body = () => (
+  <p className="App-intro">
+    To get started, edit <code>src/App.js</code> and save to reload.
+  </p>
+);
+
+// const Body = () => {
+//   return (
+//     <p className="App-intro">
+//       To get started, edit <code>src/App.js</code> and save to reload.
+//     </p>
+//   );
+// };
+
+// function Body() {
+//   return (
+//     <p className="App-intro">
+//       To get started, edit <code>src/App.js</code> and save to reload.
+//     </p>
+//   );
+// }
+
+class Header extends Component {
+  render() {
+    return (
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">Welcome to React</h1>
       </header>
-    </div>
-  );
+    );
+  }
+}
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Body />
+      </div>
+    );
+  }
 }
 
 export default App;
