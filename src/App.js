1101449b5d14fc9c65e9860b42d1b6f22b27bca3
Genesis Gabiola <gbgabiola@gmail.com>
2021-08-18 22:57:45 +0800

    Import and export components

--- src/App.js
+++ src/App.js
@@ -1,42 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Header } from './components/Header';
+import Body, { Body2, Body3 } from './components/Body';
 
-const Body = props => (
-  <div>
-    <p className="App-intro">{props.text}</p>
-    <p className="App-intro">{props.text2}</p>
-    <p className="App-intro">{props.myFunc(1, 2)}</p>
-  </div>
-);
-
-// const Body = () => {
-//   return (
-//     <p className="App-intro">
-//       To get started, edit <code>src/App.js</code> and save to reload.
-//     </p>
-//   );
-// };
-
-// function Body() {
-//   return (
-//     <p className="App-intro">
-//       To get started, edit <code>src/App.js</code> and save to reload.
-//     </p>
-//   );
-// }
-
-class Header extends Component {
-  render() {
-    return (
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">{this.props.title}</h1>
-        <div>{this.props.myFunc(10, 12)}</div>
-      </header>
-    );
-  }
-}
 class App extends Component {
   add(a, b) {
     return a + b;
@@ -57,6 +23,7 @@ class App extends Component {
             b: 6,
           }}
         />
+        <Body2 />
         <Body myFunc={this.add} text={'I am cool.'} text2={'I am cool2.'} />
         <Body myFunc={this.add} text={'I am cool.'} text2={'I am cool2.'} />
       </div>
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+
+export class Header extends Component {
+  render() {
+    return (
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">{this.props.title}</h1>
+        <div>{this.props.myFunc(10, 12)}</div>
+      </header>
+    );
+  }
+}
