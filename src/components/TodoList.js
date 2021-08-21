3acf26caed68dbac14be5d17a6b48ef0cc7db953
Genesis Gabiola <gbgabiola@gmail.com>
2021-08-21 22:46:11 +0800

    Add todo and install uuid

---
+++ src/components/TodoList.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import TodoForm from './TodoForm';
+
+/*
+  [x] 1. Add todo
+  [ ] 2. Display todos
+  [ ] 3. Cross off todo
+  [ ] 4. Show number of active todos
+  [ ] 5. Filter all/active/complete todos
+  [ ] 6. Delete todo
+  [ ] 7. Delete all complete
+        7.1 Only show if at least one is complete
+  [ ] 8. Toggle all on/off
+
+*/
+
+export default class TodoList extends Component {
+  state = {
+    todos: [],
+  };
+
+  addTodo = todo => {
+    this.setState({
+      todos: [todo, ...this.state.todos],
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <TodoForm onSubmit={this.addTodo} />
+        {JSON.stringify(this.state.todos)}
+      </div>
+    );
+  }
+}
