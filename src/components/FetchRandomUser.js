8a305652d50521784f6d155f58fc63eb09ceba4a
Genesis Gabiola <gbgabiola@gmail.com>
2021-08-21 20:09:24 +0800

    Fetch data from an api

---
+++ src/components/FetchRandomUser.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+export default class FetchRandomUser extends Component {
+  state = {
+    loading: true,
+    person: null,
+  };
+
+  async componentDidMount() {
+    const url = 'https://api.randomuser.me/';
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({ person: data.results[0], loading: false });
+  }
+
+  render() {
+    if (this.state.loading) {
+      <div>loading...</div>;
+    }
+
+    if (!this.state.person) {
+      return <div>Failed to fetch a person's data.</div>;
+    }
+
+    return (
+      <div>
+        <div>
+          <div>{this.state.person.name.title}</div>
+          <div>{this.state.person.name.first}</div>
+          <div>{this.state.person.name.last}</div>
+          <img src={this.state.person.picture.large} alt="Profile" />
+        </div>
+
+        {/* {this.state.loading || !this.state.person ? (
+          <div>loading...</div>
+        ) : (
+          <div>
+            <div>{this.state.person.name.title}</div>
+            <div>{this.state.person.name.first}</div>
+            <div>{this.state.person.name.last}</div>
+            <img src={this.state.person.picture.large} alt="Profile" />
+          </div>
+        )} */}
+      </div>
+    );
+  }
+}
