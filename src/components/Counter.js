ea4bc565b7835283b039546af1e5745d3dfc978b
Genesis Gabiola <gbgabiola@gmail.com>
2021-08-21 21:30:11 +0800

    Lift state to parent component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,36 +1,12 @@
 import React, { Component } from 'react';
 
 export default class Counter extends Component {
-  state = {
-    count: 0,
-  };
-
-  componentWillUnmount() {
-    console.log('unmounting...');
-  }
-
-  componentDidMount() {
-    console.log('mounting...');
-  }
-
-  handleIncrement = () => {
-    this.setState({
-      count: this.state.count + 1,
-    });
-  };
-
-  handleDecrement = () => {
-    this.setState({
-      count: this.state.count - 1,
-    });
-  };
-
   render() {
     return (
       <div>
-        <div>count: {this.state.count}</div>
-        <button onClick={this.handleIncrement}>increment</button>
-        <button onClick={this.handleDecrement}>decrement</button>
+        <div>count: {this.props.count}</div>
+        <button onClick={this.props.increment}>increment</button>
+        <button onClick={this.props.decrement}>decrement</button>
       </div>
     );
   }
