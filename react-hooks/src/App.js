f1794a562b3e8ccac7c422258827986aed058ac0
Genesis Gabiola <gbgabiola@gmail.com>
2021-08-22 13:00:22 +0800

    Use Multiple useState or single useState with object

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,5 +1,36 @@
+import React, { useState } from 'react';
+
 const App = () => {
-  return <div>Hello World</div>;
+  // const [count, setCount] = useState(10);
+  // const [{ count, count2 }, setCount] = useState({ count: 10, count2: 20 });
+  const [count, setCount] = useState(10);
+  const [count2, setCount2] = useState(20);
+
+  return (
+    <div>
+      {/* <button onClick={() => setCount(count => count + 1)}>+</button> */}
+      <button
+        onClick={() => {
+          setCount(count => count + 1);
+          setCount2(count => count + 1);
+        }}
+      >
+        +
+      </button>
+      {/* <button
+        onClick={() =>
+          setCount(currentState => ({
+            ...currentState,
+            count: currentState.count + 1,
+          }))
+        }
+      >
+        +
+      </button> */}
+      <div>count 1: {count}</div>
+      <div>count 2: {count2}</div>
+    </div>
+  );
 };
 
 export default App;
