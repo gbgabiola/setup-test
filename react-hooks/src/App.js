dadcbe6ad3dbd56502907ec253f17bcce3f5910e
Genesis Gabiola <gbgabiola@gmail.com>
2021-08-22 15:58:17 +0800

    Update useState hooks with each field

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,34 +1,22 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  // const [count, setCount] = useState(10);
-  // const [{ count, count2 }, setCount] = useState({ count: 10, count2: 20 });
-  const [count, setCount] = useState(10);
-  const [count2, setCount2] = useState(20);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   return (
     <div>
-      {/* <button onClick={() => setCount(count => count + 1)}>+</button> */}
-      <button
-        onClick={() => {
-          setCount(count => count + 1);
-          setCount2(count => count + 1);
-        }}
-      >
-        +
-      </button>
-      {/* <button
-        onClick={() =>
-          setCount(currentState => ({
-            ...currentState,
-            count: currentState.count + 1,
-          }))
-        }
-      >
-        +
-      </button> */}
-      <div>count 1: {count}</div>
-      <div>count 2: {count2}</div>
+      <input
+        name="email"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        name="password"
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      />
     </div>
   );
 };
