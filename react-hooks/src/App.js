199ecd54febcab4e26f17bed78adac22e208fc7a
Genesis Gabiola <gbgabiola@gmail.com>
2021-08-22 18:52:45 +0800

    Add a shallow comparison of the useEffect values

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,15 +1,40 @@
+import { useEffect } from 'react';
 import { useForm } from './useForm';
 
 const App = () => {
-  const [values, handleChange] = useForm({ email: '', password: '' });
+  const [values, handleChange] = useForm({
+    email: '',
+    password: '',
+    firstName: '',
+  });
+
+  useEffect(() => {
+    console.log('Render');
+  }, [values.email, values.password]);
+
+  // useEffect(() => {
+  //   console.log('Render');
+  // }, []);
 
   return (
     <div>
-      <input name="email" value={values.email} onChange={handleChange} />
+      <input
+        name="email"
+        value={values.email}
+        placeholder="Email"
+        onChange={handleChange}
+      />
+      <input
+        name="firstName"
+        value={values.firstName}
+        placeholder="First name"
+        onChange={handleChange}
+      />
       <input
         type="password"
         name="password"
         value={values.password}
+        placeholder="Password"
         onChange={handleChange}
       />
     </div>
